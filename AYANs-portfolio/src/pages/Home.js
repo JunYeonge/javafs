40c4c75b321a54f0dc0b433031da25f1f4c45a58
junyeonge <ehdrjs1112@naver.com>
2023-09-04 17:02:55 +0900

    0904

--- AYANs-portfolio/src/pages/Home.js
+++ AYANs-portfolio/src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Herosection from '../components/Herosection';
+import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
 
 export default function Home() {
   return (
     <div>
       <Herosection />
+      <AboutSection />
+      <ServicesSection />
     </div>
   );
 }
